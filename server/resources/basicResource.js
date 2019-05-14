fa380d0bf7b6e8fba25ce7d3fb6f406953f4b41d
moaalaa <moaalaa16@gmail.com>
2019-05-14 17:08:24 +0200

    Implementing ZRealAll Todos

---
+++ server/resources/basicResource.js
@@ -0,0 +1,50 @@
+const api = {
+    /**
+     * 
+     * @param {mixed} data Data That Will Send by The Response
+     * @param {mixed} error error message/s for response
+     * @param {number} code  status code that response will have it
+     */
+    api(data, error = null, code = 200) {
+        
+        if (error) return this.error(error, code);
+
+        this.data = data;
+        this.error = error;
+        this.code = code;
+
+        return this;
+    },
+    /**
+     * 
+     * @param {Object} response Express Response Object  
+     */
+    response(response) {
+        return response.status(this.code).send({
+            data: this.data,
+            messages: this.error,
+            code: this.code,
+        })
+    },
+    /**
+     * 
+     * @param {Object} response Express Response Object  
+     */
+    res(response) {
+        return this.response(response)
+    },
+    /**
+     * @param {mixed} error error message/s for response
+     * @param {number} code  status code that response will have it
+     */
+    error(error, code = 500) {
+        this.data = [];
+        this.code = code;
+        this.error = error.message;
+        
+        return this;
+    }
+
+}
+
+module.exports = api;
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -5,16 +5,22 @@ const router = express.Router();
 // Models
 const {Todo} = require('~models/Todo');
 
+// Resource Manager
+const responder = require('~resources/basicResource')
+
 router.get('/', (req, res) => {
-    res.json("Todos")
+    
+    Todo.find()
+        .then(docs => responder.api(docs).res(res))
+        .catch(e => responder.error(e).res(res))
 })
 
 router.post('/', (req, res) => {
     const todo = new Todo(req.body);
 
     todo.save()
-        .then(doc => res.status(201).send(doc))
-        .catch(e => res.status(400).send(e))
+        .then(doc => responder.api(doc, null, 201).res(res))
+        .catch(e => responder.error(e, 400).res(res))
 });
 
 module.exports = router;
